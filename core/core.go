e9564621b974d06d264ab669f0f07b97935961ba
Equim <sayaka@ekyu.moe>
2017-12-06 00:12:14 +0800

    lint code

--- core/core.go
+++ core/core.go
@@ -8,6 +8,7 @@ import (
 	"unsafe"
 
 	"github.com/awnumar/memguard"
+
 	"golang.org/x/crypto/curve25519"
 	"golang.org/x/crypto/nacl/secretbox"
 	"golang.org/x/crypto/salsa20/salsa"
@@ -25,43 +26,49 @@ type Session interface {
 }
 
 type session struct {
-	pub         *[32]byte
-	pri         *memguard.LockedBuffer
+	pub *[32]byte
+	pri *memguard.LockedBuffer
+
 	shared      *memguard.LockedBuffer
 	sharedArray *[32]byte
+
+	nonceSeed *[24]byte
+	seq       uint16
 
 	haveShared bool
 }
 
 // NewSession create a session and generates a key pair with their memory locked.
 func NewSession() (Session, error) {
-	s := &session{
-		pub:        new([32]byte),
-		haveShared: false,
-	}
-
+	// Init private key
 	pri, err := memguard.NewImmutableRandom(32)
 	if err != nil {
 		return nil, err
 	}
 
 	priArray := (*[32]byte)(unsafe.Pointer(&pri.Buffer()[0]))
+	pub := new([32]byte)
 
-	curve25519.ScalarBaseMult(s.pub, priArray)
+	// Calculate public key
+	curve25519.ScalarBaseMult(pub, priArray)
 
-	s.pri = pri
-	return s, nil
+	return &session{
+		pub:        pub,
+		pri:        pri,
+		seq:        0,
+		haveShared: false,
+	}, nil
 }
 
 func (s *session) PublicKey() *[32]byte {
 	return s.pub
 }
 
-// Compute computes the shared secret. On success, the private key of the
-// session will be destroyed.
+// Compute computes the shared secret and shared nonceSeed.
+// On success, the private key of the session will be destroyed.
 func (s *session) Compute(pub *[32]byte) error {
 	if s.haveShared {
-		return errors.New("compute shared secret: already have shared secret")
+		return errors.New("compute: already have shared secret")
 	}
 
 	shared, err := memguard.NewMutable(32)
@@ -77,7 +84,7 @@ func (s *session) Compute(pub *[32]byte) error {
 	salsa.HSalsa20(s.sharedArray, &zeros, s.sharedArray, &salsa.Sigma)
 
 	if err := s.shared.MakeImmutable(); err != nil {
-		return fmt.Errorf("compute shared secret: %s", err)
+		return fmt.Errorf("compute: %s", err)
 	}
 
 	s.pri.Destroy()
@@ -90,6 +97,10 @@ func (s *session) Compute(pub *[32]byte) error {
 // Seal encrypts the plain text. On success, the plain text will be
 // destroyed and return the nonce+encrypted
 func (s *session) Seal(plain *memguard.LockedBuffer) ([]byte, error) {
+	if !s.haveShared {
+		return nil, errors.New("seal: no shared key")
+	}
+
 	defer plain.Destroy()
 
 	nonce := make([]byte, 24)
@@ -106,6 +117,10 @@ func (s *session) Seal(plain *memguard.LockedBuffer) ([]byte, error) {
 }
 
 func (s *session) Open(encrypted []byte) (*memguard.LockedBuffer, error) {
+	if !s.haveShared {
+		return nil, errors.New("open: no shared key")
+	}
+
 	if len(encrypted) < 24+secretbox.Overhead {
 		return nil, errors.New("open: wrong size")
 	}
